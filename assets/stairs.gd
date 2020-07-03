df625f8f352e7a9f680ab2de5428cb327cbd736a
pete1 <16347-pete1@users.noreply.gitgud.io>
2020-07-03 16:44:25 -0700

    Upload New File

---
+++ assets/stairs.gd
@@ -0,0 +1,21 @@
+extends CollisionShape2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+
+	
+func _ready():
+	#get_tree().change_scene("res://assets/room3.tscn")
+	pass # Replace with function body.
+	
+#func _on_stairs_area_entered(stairs):
+		
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(_delta):
+	
+#	pass

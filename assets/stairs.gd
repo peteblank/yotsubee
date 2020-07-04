01be810df5ad257a32415766df06dae955d40250
Blank <blank@blank.com>
2020-07-05 00:31:16 +1000

    Added support for setting location/direction of player after switching scenes

--- assets/stairs.gd
+++ assets/stairs.gd
@@ -1,11 +1,15 @@
 extends CollisionShape2D
 
 export(String) var nextScenePath
+export(String) var targetLocation = ""
 
 func _ready():
 	pass
 
 
 func _on_Stair_body_entered(body):
-	print(body)
-	Global.goto_scene(nextScenePath)
+	if body.name == "player":
+		Global.entry_state = targetLocation
+		var x = get_tree().get_root().get_node("Node2D/YSort/player").get_node("AnimationTree")
+		Global.player_entry_direction = -1 * x.get("parameters/idle/blend_position")
+		Global.goto_scene(nextScenePath)

ee9a1e1fe63ee804b91fce2ad2c64d109036e86e
Blank <blank@blank.com>
2020-07-05 16:11:07 +1000

    Added south exit tile to tilemap and changed house filtering type.

--- assets/basicCamera.gd
+++ assets/basicCamera.gd
@@ -6,7 +6,7 @@ func _ready():
 	self.target = get_tree().get_root().get_node("Node2D/YSort/player")
 
 
-func _process(delta):
+func _process(_delta):
 	var newPos = target.position
 	#newPos.x = round(newPos.x)
 	#newPos.y = round(newPos.y)
